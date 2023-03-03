c1501f7ee7eab582c5186c2993c9cc231f3839fa
Shubham Gupta <shubham.gupta1@crmnext.com>
2023-03-03 10:50:09 +0530

    End of Section 6

--- client-app/src/features/activities/dashboards/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboards/ActivityDashboard.tsx
@@ -10,20 +10,23 @@ interface Props {
     selectedActivity:Activity | undefined;
     selectActivity: (id: string) => void;
     cancelSelectActivity: () => void;
-    editMode : Boolean;
+    editMode : boolean;
     openForm: (id: string) => void;
     closeForm: () => void;
     createOrEdit : (activity: Activity) => void;
     deleteActivity : (id: string) => void;
+    submitting : boolean;
 }
 export default function ActivityDashboard({activities, selectedActivity, 
-                selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity} : Props) {
+                selectActivity, cancelSelectActivity, editMode, openForm, 
+                closeForm, createOrEdit, deleteActivity, submitting} : Props) {
     return (
         <Grid>
             <Grid.Column width="10">
                 <ActivityList activities={activities} 
                     selectActivity={selectActivity}
                     deleteActivity = {deleteActivity}
+                    submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width="6">
@@ -32,7 +35,8 @@ export default function ActivityDashboard({activities, selectedActivity,
                                  cancelSelectActivity = {cancelSelectActivity}
                                  openForm = {openForm}/> }
                  { editMode && 
-                   <ActivityForm closeForm = {closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+                   <ActivityForm closeForm = {closeForm} activity={selectedActivity} 
+                                 createOrEdit={createOrEdit} submitting = {submitting}/>}
             </Grid.Column>
         </Grid>
     )
